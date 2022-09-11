5d82b6ce1ed5672eb06eeb4dffa917da23eb228a
kuzyi <aleckuzma@gmail.com>
2022-09-11 14:40:01 -0400

    grid created

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -17,12 +17,39 @@ const GamePage = ({ user, authenticated }) => {
     getLocation()
   }, [id])
 
+  const createGrid = () => {
+    let gridArray = []
+    for (let y = 0; y < 20; y++) {
+      let yClass = 'y' + y
+
+      for (let x = 0; x < 20; x++) {
+        let xClass = 'x' + x
+        let newArray = [xClass, yClass]
+        gridArray.push(newArray)
+      }
+    }
+
+    // console.log(gridArray)
+    return gridArray
+  }
+  let gridArray = createGrid()
+
   return (
-    <div className="location">
-      <button className="back" onClick={() => navigate('/LocationList')}>
-        Return to Games
-      </button>
-      <h1 className="locationBeach">{locationDetails.gameName}</h1>
+    <div>
+      <div className="location">
+        <button className="back" onClick={() => navigate('/LocationList')}>
+          Return to Games
+        </button>
+        <h1 className="locationBeach">{locationDetails.gameName}</h1>
+
+        <div class="grid-container">
+          {gridArray.map((gridxy) => (
+            <div className={'grid-item ' + gridxy[0] + ' ' + gridxy[1]}>
+              <p>{gridxy[0] + ' ' + gridxy[1]}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
